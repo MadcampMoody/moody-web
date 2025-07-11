ad97de5c4a47ded013840073da346e91ed43d5d5
chaehyunkIM <chaehhyun0803@pusan.ac.kr>
2025-07-11 21:04:11 +0900

    initial commit

---
+++ src/main/java/com/madcamp/moody/MoodyApplication.java
@@ -0,0 +1,13 @@
+package com.madcamp.moody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madcamp/moody/MoodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madcamp.moody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
