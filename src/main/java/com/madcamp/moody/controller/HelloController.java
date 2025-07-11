43204135fa6af11526e438adbd974b38ef926236
chaehyunkIM <chaehhyun0803@pusan.ac.kr>
2025-07-11 23:08:46 +0900

    update back

--- src/main/java/com/madcamp/moody/controller/HelloController.java
+++ src/main/java/com/madcamp/moody/controller/HelloController.java
@@ -1,11 +1,10 @@
-package com.madcamp.moody;
+package com.madcamp.moody.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class HelloController {
 
     @GetMapping("/api/test")
